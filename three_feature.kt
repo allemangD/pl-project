7766873070c1469ecb33bb752cb0f2f445e111d1
Jacob Raymond <raymond.jacob17@gmail.com>
2018-10-11 16:00:47 -0400

    Added IO

---
+++ three_feature.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+fun main(args: Array<String>) {
+   val in1 = readLine() ?: ""
+   // or
+   val reader = Scanner(System.`in`)
+
+   val in2: Int = reader.nextInt()
+
+   println(in1)
+
+   print(in2)
+
+}
