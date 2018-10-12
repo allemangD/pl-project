f1919677de50565f89aa874161aef02e2372628e
allemangD <allemang.d@gmail.com>
2018-10-11 20:45:40 -0400

    initial commit

---
+++ src/three_feature.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val in1 = readLine() ?: ""
+
+    val reader = Scanner(System.`in`)
+    val in2 = reader.nextInt()
+
+    println(in1)
+    print(in2)
+}
